c2c6dccbcffecd8eb7b5b54f9f16f42a23d116dc
Carlos Dominguez <sahrlybip/@gmail.com>
2023-06-09 17:51:38 -0600

    Updates

--- index.579125c3.js
+++ index.579125c3.js
@@ -1,39 +1,48 @@
 const app = document.getElementById("app");
 const btn_mas = document.getElementById("mas");
-console.log("uyyy");
+const imgContainer = document.getElementsByClassName("img-container");
 const main = async (page)=>{
     const baseUrl = `https://api.escuelajs.co/api/v1/products?offset=${page}&limit=10`;
     const response = await fetch(baseUrl);
     const products = await response.json();
     console.log(products);
-    const output = products?.map((product)=>{
-        return `
+    const noMore = document.createElement("p");
+    noMore.className = "noMore";
+    noMore.textContent = "No hay mas productos que mostrar...";
+    if (products.length === 0) {
+        if (!document.querySelector(".noMore")) document.body.append(noMore);
+        else return;
+    } else {
+        const output = products?.map((product)=>{
+            const images = product.images.map((img)=>{
+                return `<img class="img" src="${img}">`;
+            }).join();
+            return `
             <article class="Card">
-                <div class="img-container">
-                    <img class="img" src="${product.images[0]}">
-                    <img class="img" src="${product.images[1]}">
-                    <img class="img" src="${product.images[2]}">
-                </div>
-                <small>$ ${product.price}  <span>${Math.floor(Math.random() * 10)}% Emvio gratis</span></small>
-                <h2>
-                    ${product.title}
-                </h2>
-                <p class="visible">${product.description}</p>
+            <div class="img-container">
+            ${images}
+            </div>
+            <small>$ ${product.price}  <span>${Math.floor(Math.random() * 10)}% Emvio gratis</span></small>
+            <h2>
+            ${product.title}
+            </h2>
+            <p class="visible">${product.description}</p>
             </article>
-        `;
-    }).join("");
-    let newItem = document.createElement("section");
-    newItem.className = "Items";
-    newItem.innerHTML = await output;
-    const card = document.getElementsByClassName("Card");
-    app.appendChild(newItem);
-    return page;
+            `;
+        }).join("");
+        let newItem = document.createElement("section");
+        newItem.className = "Items";
+        newItem.innerHTML = await output;
+        newItem.addEventListener("click", (event)=>{});
+        app.appendChild(newItem);
+    }
 };
 main(0);
-btn_mas.addEventListener("click", ()=>{
+main(10);
+document.onscrollend = (event)=>{
     const page = document.querySelectorAll("article");
     const total = page.length;
     main(total);
-});
+};
 
 //# sourceMappingURL=index.579125c3.js.map
