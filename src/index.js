19ada71e7dbc80c25db42dad4dbd3fa49fdba494
Carlos Dominguez <sahrlybip/@gmail.com>
2023-06-10 12:13:01 -0600

    cambios

--- src/index.js
+++ src/index.js
@@ -1,36 +1,61 @@
 
 
 const app = document.getElementById('app');
+const btn_mas = document.getElementById('mas');
+const imgContainer = document.getElementsByClassName('img-container');
 
-const baseUrl = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
-console.log("uyyy");
 
-const main = async() => {
+const main = async(page) => {
+    const baseUrl = `https://api.escuelajs.co/api/v1/products?offset=${page}&limit=10`;
     const response =  await fetch(baseUrl);
     const products = await response.json();
-
-    const output =  products?.map(product => {
-        return `
+    console.log(products);
+    const noMore = document.createElement('p')
+    noMore.className = 'noMore';
+    noMore.textContent = "No hay mas productos que mostrar..."
+    if (products.length === 0){
+        if (!document.querySelector('.noMore')) {
+            document.body.append(noMore)
+        } else {
+            return
+        }
+    } else {
+        const output =  products?.map(product => {
+        
+            const images = product.images.map(img=>{
+                return `<img class="img" src="${img}">`;
+            }).join(); 
+            return `
             <article class="Card">
-                <div class="img-container">
-                    <img class="img" src="${product.images[0]}">
-                    <img class="img" src="${product.images[1]}">
-                    <img class="img" src="${product.images[2]}">
-                
-                </div>
-                <small>$ ${product.price}</small>
-                <h2>
-                    ${product.title}
-                </h2>
+            <div class="img-container">
+            ${images}
+            </div>
+            <small>$ ${product.price}  <span>${Math.floor(Math.random()*10)}% Envio gratis</span></small>
+            <h2>
+            ${product.title}
+            </h2>
+            <p class="visible">${product.description}</p>
             </article>
-        `;
-    }).join('');
+            `;
+        }).join('');
+        
 
     let newItem = document.createElement('section');
     newItem.className = "Items";
     newItem.innerHTML = await output;
-    
+    newItem.addEventListener('click', (event) =>{
+        
+    })
     app.appendChild(newItem);
-
 }
-main();
+}
+
+main(0);
+main(10);
+
+document.onscrollend = (event) => {
+    const page = document.querySelectorAll('article');
+    const total = page.length;
+    main(total)
+  };
+  
